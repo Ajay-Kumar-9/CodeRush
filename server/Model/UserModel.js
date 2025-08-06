02b2ef7f52ae3e849fc75978e1f96d8a59e10a48
Ajay-Kumar-9 <161349732+Ajay-Kumar-9@users.noreply.github.com>
2025-08-06 14:30:08 +0530

    changes

--- server/Model/UserModel.js
+++ server/Model/UserModel.js
@@ -1,6 +1,6 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"; 
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({ //creating a schema
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
