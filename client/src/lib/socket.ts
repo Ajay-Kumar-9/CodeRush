61c306893fee4504716c22febffb5ae632524b1a
Ajay-Kumar-9 <161349732+Ajay-Kumar-9@users.noreply.github.com>
2025-12-25 15:58:38 +0530

    changes in render url

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -7,7 +7,7 @@ export const getSocket = (): Socket => {
   if (!socket) {
     /*  socket = io(process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:5000', { */
 
-    socket = io("https://coderush-mr1i.onrender.com", {
+    socket = io("https://coderush-tvf3.onrender.com", {
       transports: ["websocket"],
       autoConnect: true,
       reconnectionAttempts: 5,
